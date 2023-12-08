3933eb68f01c7af4034ab5a0836f10193de752d4
Arkadiusz Słowikowski <slowikowskiarkadiusz@gmail.com>
2023-12-08 07:51:01 +0100

    Improved context-menu functionality and reusability
    Refactored context-menu component to increase reusability and scalability. Introduced React hooks 'useState' and 'useRef' for improving state and reference management. Implemented 'run' function to handle different mouse events and specifically introduced nested items handling during 'mouse over' events.
    
    Defined and used 'ContextMenuItem' interface for a more structured and reusable context menu items. The major change involves replacing props based handling for visibility and positioning elements with state-based control. This change promotes scalability and better control over the component's behavior."

--- src/renderer/common/context-menu.tsx
+++ src/renderer/common/context-menu.tsx
@@ -1,10 +1,46 @@
 import './context-menu.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState } from "react";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-export function ContextMenu(props: any) {
+export interface ContextMenuItem {
+    icon: IconDefinition;
+    label: string;
+    action?: () => void;
+    nested?: ContextMenuItem[];
+}
+
+export function ContextMenu() {
+    const [isVisible, setIsVisible] = useState(false);
+    const [top, setTop] = useState(0);
+    const [left, setLeft] = useState(0);
+    const [items, setItems] = useState([]);
+
+    const run = (event: MouseEvent, items: ContextMenuItem[]) => {
+        this.isVisible = (!this.isVisible && items?.length > 0);
+        this.items = items;
+
+        if (this.isVisible) {
+            let widthDiff = event.pageX - document.getElementsByTagName('body')[0]?.scrollWidth;
+            if (widthDiff > 0)
+                this.left = event.pageX - widthDiff;
+            else
+                this.left = event.pageX;
+
+            let heightDiff = event.pageY - document.getElementsByTagName('body')[0]?.scrollHeight;
+            if (heightDiff > 0)
+                this.top = event.pageY - heightDiff;
+            else
+                this.top = event.pageY;
+
+            this.left++;
+            this.top++;
+        }
+    }
+
     return <div style={ {
-        visibility: props.isVisible ? 'visible' : 'hidden',
+        visibility: isVisible ? 'visible' : 'hidden',
         pointerEvents: 'none',
         cursor: 'default',
         position: 'absolute',
@@ -13,47 +49,106 @@ export function ContextMenu(props: any) {
         maxWidth: '100%',
         top: '0',
         left: '0',
-    } }
-                onClick={ () => onBackgroundClick($event) }>
-        <ContextMenuList spawnAt={ { top: props.top, left: props.left } }
-                         items={ props.items }/>
+    } }>
+        <ContextMenuList spawnAt={ { top: top, left: left } }
+                         items={ items }/>
     </div>
 }
 
-export function ContextMenuList(props: any) {
-    return <div className="context-menu"
-                style={ {
-                    pointerEvents: 'all',
-                    top: props.spawnAt.top + 'px',
-                    left: props.spawnAt.left + 'px'
-                } }>
-        { props.items.map((item: any) =>
-            <div className={ 'context-menu-row ' + props.itemMouseIsOver?.item == item ? 'has-active-child' : '' }
-                // #row
-                 onClick={ $event => onItemClick($event, item) }
-                 onMouseEnter={ () => onMouseEnter(row, item) }
-                 onMouseLeave={ () => onMouseLeave() }>
-                <div className="left-cell context-menu-cell">
-                    <FontAwesomeIcon className="icon-content"
-                                     icon={ item.icon }/>
-                </div>
+export function ContextMenuList(props: {
+    items: ContextMenuItem[],
+    spawnAt: { top: number, left: number },
+    onmouseover?: () => void,
+    onmouseleave?: () => void
+}) {
+    const goToNestedIcon: IconDefinition = faChevronRight;
+    const nextMenuTimeout: number = 500;
+    const [nextMenuItems, setNextMenuItems] = useState(undefined as ContextMenuItem[]);
+    const [nextMenuAt, setNextMenuAt] = useState(undefined as { top: number, left: number });
+    const [isMouseOverChild, setIsMouseOverChild] = useState(false);
+    const [itemMouseIsOver, setItemMouseIsOver] = useState(undefined as { div: HTMLDivElement, item: ContextMenuItem });
+    const [mouseOverTimeout, setMouseOverTimeout] = useState(undefined as number);
 
-                <div className="middle-cell context-menu-cell">
+    const onItemClick = ($event: MouseEvent, item: ContextMenuItem) => {
+        if (item.action)
+            item.action();
+        else if (item.nested && item.nested.length > 0 && item.nested[0].action)
+            item.nested[0].action();
+        else
+            $event.stopImmediatePropagation();
+    }
+
+    const onMouseEnter = (div: HTMLDivElement, item: ContextMenuItem): void => {
+        if (props.onmouseover)
+            props.onmouseover();
+        setItemMouseIsOver({ div, item });
+
+        if (mouseOverTimeout) {
+            clearTimeout(mouseOverTimeout);
+            setMouseOverTimeout(mouseOverTimeout);
+        }
+
+        setMouseOverTimeout(setTimeout(() => {
+            let element = div.lastElementChild as HTMLDivElement;
+            if (itemMouseIsOver && (item.nested?.length ?? 0) > 0) {
+                setNextMenuAt({
+                    top: props.spawnAt.top + element!.offsetTop,
+                    left: props.spawnAt.left + element!.offsetLeft + element!.offsetWidth
+                });
+                setNextMenuItems(item.nested);
+            } else {
+                setNextMenuAt(undefined);
+                setNextMenuItems(undefined);
+            }
+
+            setMouseOverTimeout(undefined);
+        }, nextMenuTimeout) as any as number);
+    }
+
+    const refs = new Array(props.items.length).fill(useRef(null));
+
+    return <>
+        <div className="context-menu"
+             style={ {
+                 pointerEvents: 'all',
+                 top: props.spawnAt.top + 'px',
+                 left: props.spawnAt.left + 'px'
+             } }>
+            { props.items.map((item, i) =>
+                <div key={ `context-menu-row-${ i }` }
+                     ref={ refs[i] }
+                     className={ 'context-menu-row ' + (itemMouseIsOver?.item == item ? 'has-active-child' : '') }
+                     onClick={ $event => onItemClick($event.nativeEvent, item) }
+                     onMouseEnter={ () => onMouseEnter(refs[i], item) }
+                     onMouseLeave={ () => {if (props.onmouseleave) props.onmouseleave()} }>
+                    <div className="left-cell context-menu-cell">
+                        <FontAwesomeIcon className="icon-content"
+                                         icon={ item.icon }/>
+                    </div>
+
+                    <div className="middle-cell context-menu-cell">
                     <span className="label-content">
                         { item.label }
                     </span>
-                </div>
-
-                { (item.nested?.length ?? 0) == 0
-                    ? <div className="right-cell context-menu-cell"></div>
-                    : undefined }
-
-                { (item.nested?.length ?? 0) > 0
-                    ? <div className="right-cell context-menu-cell">
-                        <FontAwesomeIcon className="icon-content"
-                                         icon={ faChevronRight }/>
                     </div>
-                    : undefined }
-            </div>) }
-    </div>
+
+                    { (item.nested?.length ?? 0) == 0
+                        ? <div className="right-cell context-menu-cell"></div>
+                        : undefined }
+
+                    { (item.nested?.length ?? 0) > 0
+                        ? <div className="right-cell context-menu-cell">
+                            <FontAwesomeIcon className="icon-content"
+                                             icon={ faChevronRight }/>
+                        </div>
+                        : undefined }
+                </div>) }
+        </div>
+        { (nextMenuItems && nextMenuAt) || isMouseOverChild
+            ? <ContextMenuList items={ nextMenuItems }
+                               spawnAt={ nextMenuAt }
+                               onmouseover={ () => setIsMouseOverChild(true) }
+                               onmouseleave={ () => setIsMouseOverChild(false) }/>
+            : undefined }
+    </>
 }

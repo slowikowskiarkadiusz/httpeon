edb7cdb00d97cd9a3d8c368426f292b28b8fb31d
Arkadiusz Słowikowski <slowikowskiarkadiusz@gmail.com>
2023-12-22 16:08:55 +0100

    Reworked endpoint-text-editor.tsx to a class component

--- src/renderer/pages/endpoints/endpoint-text-editor.tsx
+++ src/renderer/pages/endpoints/endpoint-text-editor.tsx
@@ -1,99 +1,155 @@
-import { useState } from "react";
+import React, { Component } from 'react';
 
-const displayModes = ['default', 'json']
+const displayModes = ['default', 'json'];
 type DisplayMode = typeof displayModes[number];
 
-export function EndpointTextEditor(props: { data: { [p: string]: string } }) {
-    const [currentTab, setCurrentTab] = useState(Object.keys(props.data)[0])
-    const [displayMode, setDisplayMode] = useState<DisplayMode>('default');
+interface EndpointTextEditorProps {
+    data: { [p: string]: string };
+}
 
-    return <div style={ {
-        height: '100%',
-        borderRadius: 'var(--border-radius)',
-        overflow: 'hidden',
-        display: 'grid',
-        gridTemplateRows: 'min-content auto min-content',
-        backgroundColor: 'var(--theme-bc-3)',
-    } }>
-        <div style={ {
-            display: 'flex',
-            width: '100%',
-            overflow: 'scroll',
-            minHeight: '3em',
-        } }>
-            { Object.keys(props.data).map((key, i, c) => <div
-                key={ `page-tab-${ i }` }
-                style={ {
-                    width: `${ 100 / c.length }%`,
-                    backgroundColor: currentTab === key ? 'var(--theme-bc-3)' : 'var(--theme-bc-2)',
-                    transition: 'width 0.2s ease-out, background-color 0.2s',
-                    cursor: 'pointer',
-                    flex: 1,
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    padding: '0.5em',
-                } }
-                onClick={ () => setCurrentTab(key) }>
-                <button style={ {
-                    backgroundColor: 'transparent', color: 'unset', border: 'none', cursor: 'pointer', textAlign: 'center', width: '100%', overflow: 'hidden',
-                } }>
-                    { key }
-                </button>
-            </div>) }
-        </div>
-        <div style={ { height: '100%', overflow: 'hidden' } }>
-            { renderContent(props, currentTab, displayMode) }
-        </div>
-        <div>
-            <select style={ {
+interface EndpointTextEditorState {
+    currentTab: string;
+    displayMode: DisplayMode;
+}
+
+export class EndpointTextEditor extends Component<EndpointTextEditorProps, EndpointTextEditorState> {
+    constructor(props: EndpointTextEditorProps) {
+        super(props);
+        this.state = {
+            currentTab: Object.keys(props.data)[0],
+            displayMode: 'default',
+        };
+    }
+
+    render() {
+        const { data } = this.props;
+        const { currentTab, displayMode } = this.state;
+
+        return (
+            <div style={ {
                 height: '100%',
-                border: 'none',
-                fontSize: '2rem',
-                color: 'var(--theme-font-color)',
-                backgroundColor: 'var(--theme-bc-2)',
-                padding: '0.5em',
-                textAlign: 'center',
-            } }
-                    onChange={ (e) => setDisplayMode((e.nativeEvent.target as HTMLInputElement).value as DisplayMode) }>
-                { displayModes.map(x => <option value={ x }
-                                                key={ x }>{ x }</option>) }
-            </select>
-        </div>
-    </div>
-}
-
-function renderContent(props: { data: { [p: string]: string } }, currentTab: string, mode: DisplayMode,) {
-    switch (mode) {
-        case 'default':
-            return renderContentAsDefault(props, currentTab);
-            break;
-        case 'json':
-            return renderContentAsJson(props, currentTab);
+                borderRadius: 'var(--border-radius)',
+                overflow: 'hidden',
+                display: 'grid',
+                gridTemplateRows: 'min-content auto min-content',
+                backgroundColor: 'var(--theme-bc-3)',
+            } }>
+                <div style={ {
+                    display: 'flex',
+                    width: '100%',
+                    overflow: 'scroll',
+                    minHeight: '3em',
+                } }>
+                    { Object.keys(data).map((key, i, c) => (
+                        <div
+                            key={ `page-tab-${ i }` }
+                            style={ {
+                                width: `${ 100 / c.length }%`,
+                                backgroundColor: currentTab === key ? 'var(--theme-bc-3)' : 'var(--theme-bc-2)',
+                                transition: 'width 0.2s ease-out, background-color 0.2s',
+                                cursor: 'pointer',
+                                flex: 1,
+                                whiteSpace: 'nowrap',
+                                overflow: 'hidden',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                padding: '0.5em',
+                            } }
+                            onClick={ () => this.setCurrentTab(key) }
+                        >
+                            <button
+                                style={ {
+                                    backgroundColor: 'transparent',
+                                    color: 'unset',
+                                    border: 'none',
+                                    cursor: 'pointer',
+                                    textAlign: 'center',
+                                    width: '100%',
+                                    overflow: 'hidden',
+                                } }
+                            >
+                                { key }
+                            </button>
+                        </div>
+                    )) }
+                </div>
+                <div style={ { height: '100%', overflow: 'hidden' } }>{ this.renderContent() }</div>
+                <div>
+                    <select
+                        style={ {
+                            height: '100%',
+                            border: 'none',
+                            fontSize: '2rem',
+                            color: 'var(--theme-font-color)',
+                            backgroundColor: 'var(--theme-bc-2)',
+                            padding: '0.5em',
+                            textAlign: 'center',
+                        } }
+                        onChange={ (e) => this.setDisplayMode((e.nativeEvent.target as HTMLInputElement).value as DisplayMode) }
+                    >
+                        { displayModes.map((x) => (
+                            <option value={ x }
+                                    key={ x }>
+                                { x }
+                            </option>
+                        )) }
+                    </select>
+                </div>
+            </div>
+        );
     }
-}
 
-function renderContentAsDefault(props: { data: { [p: string]: string } }, currentTab: string) {
-    return <></>;
-}
+    setCurrentTab = (tab: string) => {
+        this.setState({ currentTab: tab });
+    };
 
-function renderContentAsJson(props: { data: { [p: string]: string } }, currentTab: string) {
-    let content = '';
-    try {
-        content = JSON.stringify(JSON.parse(props.data[currentTab]), null, 2);
-    } catch (err) {
-        content = props.data[currentTab];
+    setDisplayMode = (mode: DisplayMode) => {
+        this.setState({ displayMode: mode });
+    };
+
+    renderContent() {
+        const { data } = this.props;
+        const { currentTab, displayMode } = this.state;
+
+        switch (displayMode) {
+            case 'default':
+                return this.renderContentAsDefault();
+            case 'json':
+                return this.renderContentAsJson();
+            default:
+                return null;
+        }
     }
-    return <textarea style={ {
-        height: '100%',
-        width: '100%',
-        color: 'var(--theme-font-color)',
-        backgroundColor: 'var(--theme-bc-3)',
-        border: 'none',
-        padding: '1em'
-    } }
-                     defaultValue={ content }
-                     key={ content }
-                     onChange={ e => props.data[currentTab] = e.target.value }></textarea>
+
+    renderContentAsDefault() {
+        return <></>;
+    }
+
+    renderContentAsJson() {
+        const { data } = this.props;
+        const { currentTab } = this.state;
+
+        let content = '';
+        try {
+            content = JSON.stringify(JSON.parse(data[currentTab]), null, 2);
+        } catch (err) {
+            content = data[currentTab];
+        }
+
+        return (
+            <textarea
+                style={ {
+                    height: '100%',
+                    width: '100%',
+                    color: 'var(--theme-font-color)',
+                    backgroundColor: 'var(--theme-bc-3)',
+                    border: 'none',
+                    padding: '1em',
+                } }
+                defaultValue={ content }
+                key={ content }
+                onChange={ (e) => (data[currentTab] = e.target.value) }
+            ></textarea>
+        );
+    }
 }
